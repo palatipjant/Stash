30df927c17df49d02bdc9f60f7bfcea9cb49b4b9
palatipjant <palatipjant@outlook.com>
2024-03-08 15:19:31 +0700

    Initial Commit

---
+++ Stash/StashApp.swift
@@ -0,0 +1,17 @@
+//
+//  StashApp.swift
+//  Stash
+//
+//  Created by Palatip Jantawong on 8/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct StashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
